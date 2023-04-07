ae13a3832aaed71b9fed6968c577519d68b29e59
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-04-07 22:11:09 +0530

    Observing correlation between 3 hyperparameters + 1 hyperparameter through color

--- Implementation/version8 - data and Graphs/Dash-Plotly Apps/try.py
+++ Implementation/version8 - data and Graphs/Dash-Plotly Apps/try.py
@@ -1,28 +1,14 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from operator import imod
+from dash import Dash, dcc, html
 
 app = Dash(__name__)
 
-my_input = dcc.Input(value='initial value', type='text')
-my_output = html.Div()
-
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        my_input
-    ]),
-    html.Br(),
-    my_output
+    dcc.ConfirmDialog(
+        id='confirm',
+        message='Danger danger! Are you sure you want to continue?'
+    )
 ])
-
-
-@callback(
-    Output(my_output, component_property='children'),
-    Input(my_input, component_property='value')
-)
-def update_output_div(input_value):
-    return f'Output: {input_value}'
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
