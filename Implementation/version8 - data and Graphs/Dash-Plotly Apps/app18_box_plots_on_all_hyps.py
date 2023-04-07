56a59d3130bc69fcd8af87515dac684eb171c74f
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-04-07 23:52:06 +0530

    Box Plots to compare the distribution of the values chosen for that hypeparameter

---
+++ Implementation/version8 - data and Graphs/Dash-Plotly Apps/app18_box_plots_on_all_hyps.py
@@ -0,0 +1,89 @@
+import winsound
+import pyttsx3
+
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import plotly.graph_objs as go
+import plotly.express as px
+
+import pandas as pd
+
+import optuna
+import urllib
+
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-30) 
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def study_loader():
+    study_name = "study-26"
+    url = "https://raw.githubusercontent.com/SBhat2615/AutoPilotRL/main/Implementation/version8%20-%20data%20and%20Graphs/study-26.db"
+    storage_name = f"sqlite:///{study_name}.db"
+
+    with urllib.request.urlopen(url) as response:
+        with open(study_name + ".db", 'wb') as f:
+            f.write(response.read())
+
+    study = optuna.load_study(
+        study_name=study_name,
+        storage=storage_name
+    )
+
+    return study
+
+study = study_loader()
+
+def preprocess_df(study, normalize=True):
+    trials_df = study.trials_dataframe()
+
+    for i in ['datetime_start', 'datetime_complete']:
+        trials_df[i] = trials_df[i].apply(lambda x: int(x.timestamp()))
+
+    trials_df['duration'] = trials_df['duration'].dt.total_seconds()
+    trials_df = trials_df.drop('state', axis=1)
+
+    if(normalize):
+        trials_df = trials_df.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+    return trials_df
+
+trials_df = preprocess_df(study, True)
+
+
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H4("Box Plots of the Hyperparameter Configurations"),
+        html.P("x-axis:"),
+        dcc.Checklist(
+            id="x-axis",
+            options=trials_df.columns.to_list(),
+            value=trials_df.columns.to_list(),
+            inline=True,
+        ),
+        dcc.Graph(id="graph"),
+    ]
+)
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("x-axis", "value"),
+)
+def update_graph(column_name):
+    fig = px.box(trials_df, y=column_name)
+
+    fig.update_xaxes(title_text="Hyperparameters")
+    fig.update_yaxes(title_text="Distribution")
+    return fig
+
+
+winsound.Beep(440, 500)
+speak('Dash App Updated !')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
