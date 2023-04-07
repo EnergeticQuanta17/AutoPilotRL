7769a7bc335818a6698e532cb51d02ce2b4aa45f
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-04-07 11:10:52 +0530

    automating using git_push.py

---
+++ Implementation/version8 - data and Graphs/git_push.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.run(["python", "remove.py"])
+
+subprocess.run(["git", "add", "."])
+commit_message = input("Enter commit message: ")
+subprocess.run(["git", "commit", "-m", commit_message])
+subprocess.run(["git", "push"])
