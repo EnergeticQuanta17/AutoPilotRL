5425a913836bf71f9a5304f88284d9ff0331f72a
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-04-08 17:40:32 +0530

    Parallel Coordinates - TO DO - Very clearn representation

---
+++ Implementation/version8.1 - Plotly only/Plotly-Charts/chart9-parallel_coordinates-demo.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+df = px.data.iris()
+fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
+                  "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
+                  "petal_width": "Petal Width", "petal_length": "Petal Length", },
+                    color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
+fig.write_html('Parallel_Coordiantes_with_color_to_repesent_main_dimension_amazing.html', auto_open=True)
